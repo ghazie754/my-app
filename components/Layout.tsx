7a9690247fa31cbd92a4e328840e2dbcb79d01e0
Guyzie754 <67240493+ghazie754@users.noreply.github.com>
2024-01-04 00:41:59 +0000

    canvas

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Footer from './footer';
 import Hero from './hero';
-import draw from './draw';
 
 export default function Layout({ children }: { children?: any }): JSX.Element {
   const canvasRef = React.useRef<HTMLCanvasElement | any>(undefined);
-
+  const draw = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.arc(50, 100, 20, 0, 2 * Math.PI);
+    ctx.fill();
+  };
   React.useEffect(() => {
     const constext = canvasRef.current;
-    const ctx = constext.getContext('2d');
+    const ctx = constext?.getContext('2d');
     draw(ctx);
   }, []);
   return (
--- components/draw.ts
+++
@@ -1,6 +0,0 @@
-export default function draw(ctx: any) {
-  ctx.fillStyle = '#000000';
-  ctx.beginPath();
-  ctx.arc(50, 100, 20, 0, 2 * Math.PI);
-  ctx.fill();
-}
