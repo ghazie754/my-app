ea29b1166642b53159c69964ed792cb3b55b2d77
Guyzie754 <67240493+ghazie754@users.noreply.github.com>
2024-02-26 04:29:25 +0000

    farmermtin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,14 @@
 import React from 'react';
 import '@/styles/styles.scss';
 import '@/styles/globals.scss';
-import Hero from '@/components/hero';
 import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
-import Footer from '@/components/footer';
-import { AnimatePresence } from 'framer-motion';
+import Layout from '@/components/PageView';
 
-function Layout({ children }: { children?: any }): JSX.Element {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
-      <AnimatePresence initial={false} mode='popLayout'>
-        <Hero />
-        {/* <!-- main --> */}
-        {children}
-        {/* <!-- end of main --> */}
-        <Footer />
-      </AnimatePresence>
-    </>
-  );
-}
-
-export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  const pageKey = router.asPath;
-  return (
-    <>
-      <Layout>
-        <Component key={pageKey} {...pageProps} />
+      <Layout router={router} backgroundColor={'blue'}>
+        <Component {...pageProps} />
       </Layout>
     </>
   );
