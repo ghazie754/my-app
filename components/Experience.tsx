db1c7f354a375f4cd87439d973a590bcdba74c80
PAWN <pawn@Zas-MacBook.local>
2024-06-04 21:10:15 -0400

    hjsfdg

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -38,7 +38,7 @@ export default function Experience() {
   );
 }
 
-const ExperienceCard = ({ title, desc, year, company, companyLink }) => {
+const ExperienceCard = ({ title, desc, year, company, companyLink }:any) => {
   return (
     <div className="relative experience-card border p-4 rounded-md shadow-xl bg-white dark:bg-gray-800 z-10 mx-4">
       <h1 className="absolute -top-10 md:-left-10 md:-top-10 text-4xl text-gray-200 font-bold dark:text-gray-800">
